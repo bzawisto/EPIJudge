5b5c4f59e265e3d57b7f948917c92337ecf9a086
Bartosz Zawistowski <bartosz.zawistowski@baadgames.com>
2018-10-21 20:14:31 +0200

    Swap bits impl

--- epi_judge_cpp/swap_bits.cc
+++ epi_judge_cpp/swap_bits.cc
@@ -1,7 +1,10 @@
 #include "test_framework/generic_test.h"
 long long SwapBits(long long x, int i, int j) {
-  // TODO - you fill in here.
-  return 0;
+  if(((x >> i) & 0x1) != ((x >> j) & 0x1)) {
+    long long mask = (1L << i) | (1L << j);
+    x ^= mask;
+  }
+  return x;
 }
 
 int main(int argc, char* argv[]) {
